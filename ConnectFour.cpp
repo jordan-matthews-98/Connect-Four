56f1a4f745ba114d1b189d1ff90bf83066862394
jordan-matthews-98 <45404939+jordan-matthews-98@users.noreply.github.com>
2019-08-12 20:18:32 -0500

    added blocking checks to column priority algorithm

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 const int BASE_WEIGHT = 1;
 const int COMBO_WEIGHT = 4;
+const int DEFENSIVE_WEIGHT = 6;
 const char PLAYER = 15;
 const char CPU = 254;
 
@@ -139,16 +140,22 @@ int cpuChooseCol(char board[][7]) {
 	for (int col = 0; col < 7; ++col) {
 		for (int row = 0; row < 6; ++row) {
 			if (board[row][col] == ' ') {
-				int verticalCounterC = 0, verticalCounter = 0, iRow, iCol, sRow, sCol;
+				int verticalCounterD = 0, verticalCounterC = 0, verticalCounter = 0, iRow, iCol, sRow, sCol;
 				for (iRow = row + 1, sRow = min(row + 3, 5); iRow <= sRow && board[iRow][col] != PLAYER; ++iRow){
                     if(board[iRow][col] == CPU)
                         verticalCounterC += COMBO_WEIGHT;
 					verticalCounter += BASE_WEIGHT;
 				}
+				for(; iRow <= sRow && board[iRow][col] == PLAYER; ++iRow){
+                    verticalCounterD += DEFENSIVE_WEIGHT*(7-(iRow - row));
+				}
 				for (iRow = row - 1, sRow = max(row - 3, 0); iRow >= sRow && board[iRow][col] != PLAYER; --iRow){
                     if(board[iRow][col] == CPU)
                         verticalCounterC += COMBO_WEIGHT;
 					verticalCounter += BASE_WEIGHT;
+				}
+				for(; iRow >= sRow && board[iRow][col] == PLAYER; --iRow){
+                    verticalCounterD += DEFENSIVE_WEIGHT*(7-(row - iRow));
 				}
 				if (verticalCounterC >= COMBO_WEIGHT*3)
                     return col;
@@ -157,17 +164,22 @@ int cpuChooseCol(char board[][7]) {
                 else
                     verticalCounter += verticalCounterC;
 
-
-				int horizontalCounterC = 0, horizontalCounter = 0;
+				int horizontalCounterD = 0, horizontalCounterC = 0, horizontalCounter = 0;
 				for (iCol = col + 1, sCol = min(col + 3, 6); iCol <= sCol && board[row][iCol] != PLAYER; ++iCol){
                     if(board[row][iCol] == CPU)
                         horizontalCounterC += COMBO_WEIGHT;
 					horizontalCounter += BASE_WEIGHT;
 				}
+				for(; iCol <= sCol && board[row][iCol] == PLAYER; ++iCol){
+                    horizontalCounterD += DEFENSIVE_WEIGHT*(7-(iCol - col));
+				}
 				for (iCol = col - 1, sCol = max(col - 3, 0); iCol >= sCol && board[row][iCol] != PLAYER; --iCol){
                     if(board[row][iCol] == CPU)
                         horizontalCounterC += COMBO_WEIGHT;
 					horizontalCounter += BASE_WEIGHT;
+				}
+				for(; iCol >= sCol && board[row][iCol] == PLAYER; --iCol){
+                    horizontalCounterD += DEFENSIVE_WEIGHT*(7-(col - iCol));
 				}
 				if (horizontalCounterC >= COMBO_WEIGHT*3)
                     return col;
@@ -176,16 +188,22 @@ int cpuChooseCol(char board[][7]) {
                 else
                     horizontalCounter += horizontalCounterC;
 
-				int diagonalCounter1C = 0, diagonalCounter1 = 0;
+				int diagonalCounter1D = 0, diagonalCounter1C = 0, diagonalCounter1 = 0;
 				for (iRow = row + 1, iCol = col + 1, sCol = min(col + 3, 6), sRow = min(row + 3, 5); iCol <= sCol && iRow <= sRow && board[iRow][iCol] != PLAYER; ++iCol, ++iRow){
                     if(board[iRow][iCol] == CPU)
                         diagonalCounter1C += COMBO_WEIGHT;
 					diagonalCounter1 += BASE_WEIGHT;
 				}
+				for(; iCol <= sCol && iRow <= sRow && board[iRow][iCol] == PLAYER; ++iCol, ++iRow){
+                    diagonalCounter1D += DEFENSIVE_WEIGHT*(7-(iCol - col));
+				}
 				for (iRow = row - 1, iCol = col - 1, sCol = max(col - 3, 0), sRow = max(row - 3, 0); iCol >= sCol && iRow >= sRow && board[iRow][iCol] != PLAYER; --iCol, --iRow){
                     if(board[iRow][iCol] == CPU)
                         diagonalCounter1C += COMBO_WEIGHT;
 					diagonalCounter1 += BASE_WEIGHT;
+				}
+				for(; iCol >= sCol && iRow >= sRow && board[iRow][iCol] == PLAYER; --iCol, --iRow){
+                    diagonalCounter1D += DEFENSIVE_WEIGHT*(7-(col - iCol));
 				}
 				if (diagonalCounter1C >= COMBO_WEIGHT*3)
                     return col;
@@ -194,16 +212,22 @@ int cpuChooseCol(char board[][7]) {
                 else
                     diagonalCounter1 += diagonalCounter1C;
 
-				int diagonalCounter2C = 0, diagonalCounter2 = 0;
+				int diagonalCounter2D = 0, diagonalCounter2C = 0, diagonalCounter2 = 0;
 				for (iRow = row - 1, iCol = col + 1, sCol = min(col + 3, 6), sRow = max(row - 3, 0); iCol <= sCol && iRow >= sRow && board[iRow][iCol] != PLAYER; ++iCol, --iRow){
                     if(board[iRow][iCol] == CPU)
                         diagonalCounter2C += COMBO_WEIGHT;
 					diagonalCounter2 += BASE_WEIGHT;
 				}
+				for(; iCol <= sCol && iRow >= sRow && board[iRow][iCol] == PLAYER; ++iCol, --iRow){
+                    diagonalCounter2D += DEFENSIVE_WEIGHT*(7-(iCol - col));
+				}
 				for (iRow = row + 1, iCol = col - 1, sCol = max(col - 3, 0), sRow = min(row + 3, 5); iCol >= sCol && iRow <= sRow && board[iRow][iCol] != PLAYER; --iCol, ++iRow){
                     if(board[iRow][iCol] == CPU)
                         diagonalCounter2C += COMBO_WEIGHT;
 					diagonalCounter2 += BASE_WEIGHT;
+				}
+				for(; iCol >= sCol && iRow <= sRow && board[iRow][iCol] == PLAYER; --iCol, ++iRow){
+                    diagonalCounter2D += DEFENSIVE_WEIGHT*(7-(col - iCol));
 				}
 				if (diagonalCounter2C >= COMBO_WEIGHT*3)
                     return col;
@@ -212,7 +236,7 @@ int cpuChooseCol(char board[][7]) {
                 else
                     diagonalCounter2 += diagonalCounter2C;
 
-				colVals[col] = verticalCounter + horizontalCounter + diagonalCounter1 + diagonalCounter2;
+				colVals[col] = verticalCounter + horizontalCounter + diagonalCounter1 + diagonalCounter2 + max(max(max(verticalCounterD, horizontalCounterD), diagonalCounter1D), diagonalCounter2D);
 				break;
 			}
 		}
